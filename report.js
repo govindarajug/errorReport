77b2c5e721ec6c6f368d9260bf4788e30e17b729
Govinda G <99521156+govindarajug@users.noreply.github.com>
2022-05-17 12:28:03 +0530

    positioned the bars to center of the page
    added 'errorReport.html' in gitignore

--- report.js
+++ report.js
@@ -8,8 +8,8 @@ const createAttributes = function (attrName, attrValue) {
 };
 
 const openTag = (tag, attributes) => '<' + tag + ' ' + attributes + '>';
-const closeTag = (tag) => '</'+tag+'>';
-const createTag = function (tag, content, attributes='') {
+const closeTag = (tag) => '</' + tag + '>';
+const createTag = function (tag, content, attributes = '') {
  return openTag(tag, attributes) + content + closeTag(tag);
 };
 
@@ -18,30 +18,44 @@ const height = (h) => 'height:' + h + 'px';
 
 const rectangle = function (w, h) {
   const styleAttr = [width(w), height(h)].join(';');
-  return '<div style="' + styleAttr + '"></div>';
+  return createTag('div', '', createAttributes('style', styleAttr));
 };
 
 const getFileName = (filePath) => filePath.split('/').pop();
 
 const createBar = function ([filePath, errorCount]) {
-  const bar = rectangle(30,errorCount * 10);
+  const bar = rectangle(30, errorCount * 10);
   const name = createTag('div', getFileName(filePath));
   return createTag('div', createTag('div', errorCount) + bar + name);
 };
 
-const createReport = function (filePath) {
-  const errors = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  const pathAndCount = errors.map(x => [x.filePath,x.errorCount]);
-
+const createReport = function (errors) {
+  const pathAndCount = errors.map(x => [x.filePath, x.errorCount]);
+  
   let bars = pathAndCount.map(createBar);
   bars = createTag('div', bars.join(' '), createAttributes('class', 'errors'));
-
-  const linkTag = '<link href="styles.css", rel="StyleSheet" />'
+  
+  const linkTag = '<link href="styles.css", rel="StyleSheet" />';
   const headTag = createTag('head', linkTag);
-  const bodyTag = createTag('body', bars);
+  const wrapper = createTag('div', bars, createAttributes('class', 'wrapper'));
+  const bodyTag = createTag('body', wrapper);
   const html = createTag('html', headTag + bodyTag);
   return html;
 };
 
-const html = createReport('./report.json');
-fs.writeFileSync('errorReport.html', html, 'utf8');
+const main = function (filePath) {
+  let errors = [];
+  try {
+    errors = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  } catch (error) {
+    throw 'file not found';
+  }
+  const html = createReport(errors);
+  try {
+    fs.writeFileSync('errorReport.html', html, 'utf8');
+  } catch (error) {
+    throw 'cannot write to file';
+  }
+};
+
+main('./report.json');
